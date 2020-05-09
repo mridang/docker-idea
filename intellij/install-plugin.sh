7e179eff4ec27c3104cf8ce1ab740131f879bdbd
Olli Kuonanoja <olli.kuonanoja@nosto.com>
2020-05-09 16:54:54 +0300

    Support building with plugins
    The plug-ins can be set as a white-space separated build argument
    `PLUGIN_IDS`. For example to build with a scala plug-in:
    ```
    docker build --build-arg PLUGIN_IDS=1347 .
    ```

---
+++ intellij/install-plugin.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Installs given plugin
+# The plugin is defined by passing the plugin id as an argument.
+# See the scala plugin for example https://plugins.jetbrains.com/plugin/1347-scala
+# The id would be 1347.
+# This script assumes the following arguments
+# - IDEA_VERSION: The idea version, e.g. 2020.1
+# - PLUGIN_ID: The id of the plugin
+# - PLUGINS_DIR: The directory where to install the plugin
+#
+# See https://stackoverflow.com/a/55243388/11140326
+# how to get the plugin URL
+
+IDEA_VERSION=$1
+PLUGIN_ID=$2
+PLUGINS_DIR=$3
+
+echo "Installing plugin $PLUGIN_ID for $IDEA_VERSION in $PLUGINS_DIR"
+cd "$PLUGINS_DIR" || exit
+
+LATEST=$(curl -s https://plugins.jetbrains.com/api/plugins/$PLUGIN_ID/updates \
+  | jq -r '[.[] | select( .version | startswith(env.IDEA_VERSION))][0].file')
+ARCHIVE_NAME=$(basename "$LATEST")
+curl "https://plugins.jetbrains.com/files/$LATEST" > "$ARCHIVE_NAME"
+unzip $ARCHIVE_NAME
+rm $ARCHIVE_NAME
